eb3e63a327033d1161776306ac60a8ccd2e3e354
Apoo3va <apoorva.akb@gmail.com>
2026-08-12 15:07:24 +0530

    add CI pipeline (GitHub Actions) and app test script

---
+++ test_app.py
@@ -0,0 +1,17 @@
+import time
+import requests
+
+APP_URL = "http://localhost:8000"
+
+
+def get_status_code(url):
+    response = requests.get(url)
+    return response.status_code
+
+
+def test_streamlit_app_loads():
+    time.sleep(60)
+    status_code = get_status_code(APP_URL)
+    assert status_code == 200, "Unable to load the Streamlit app."
+    print("Streamlit app loaded successfully.")
+EOF
